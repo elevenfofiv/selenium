a93c324e3f4b40b063b4299326cb61d46b46eaff
elevenfofiv <elevenfofiv@outlook.com>
2022-07-08 17:19:10 +0900

    bot

--- main.py
+++ main.py
@@ -1,4 +1,22 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.edge.service import Service as EdgeService
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
 
-broswer = webdriver.Chrome()
+KEYWORD = "buy domain"
 
+drivers = webdriver.Edge(service=EdgeService(EdgeChromiumDriverManager().install()))
+drivers.get("https://google.com")
+
+search_bar = drivers.find_element(By.CLASS_NAME, "gLFyf")
+search_bar.send_keys(KEYWORD)
+search_bar.send_keys(Keys.ENTER)
+
+search_results = drivers.find_elements(By.CLASS_NAME, "g")
+
+for index, search_result in enumerate(search_results):
+    class_name = search_result.get_attribute("class")
+    search_result.screenshot(f"screenshots/{KEYWORD}x{index}.png")
+    # if "Ww4FFb tF2Cxc" not in class_name:
+    #     search_result.screenshot(f"screenshot/{KEYWORD}x{index}.png")
